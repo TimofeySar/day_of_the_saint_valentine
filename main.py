2c019edcde92e2f748dc43344d4d2f62ea69cdbf
dmitr <awd@adw>
2025-02-08 02:58:12 +0300

    123

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -28,4 +29,5 @@ def final():
     return render_template('final.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Используем порт из переменной окружения или 5000 по умолчанию
+    app.run(host='0.0.0.0', port=port)
